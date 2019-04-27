5d5ea906c0577f15ac8c745edf01a9d231383636
Tunar Mahmudov <tunardev14@gmail.com>
2019-04-27 19:04:20 +0300

    Commit 1

---
+++ Box Shooter/Assets/Scripts/My Scripts/BasicMover.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour
+{
+    public float spinSpeed = 180.0f;
+
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+    }
+
+}
